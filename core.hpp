e398ecad73e24d7482755cfb2f05db5de0144b61
thereAreDemonsNearby <shiletong@outlook.com>
2019-04-30 18:10:50 +0800

    core

--- core.hpp
+++ core.hpp
@@ -42,6 +42,11 @@ inline bool noMoreData(std::FILE* fp)
     }
 }
 
+namespace sha
+{
+std::vector<uint8_t> digest_sha256(std::FILE* src);
+}
+
 namespace rsa
 {
 template<std::size_t KeyBits>
@@ -379,3 +384,100 @@ BigUInt<KeyBits> decryptUsingChineseRemainderTheorem(
 }
 
 } // namespace rsa
+
+namespace sha
+{
+namespace detail
+{
+
+uint32_t rightRotate(uint32_t v, std::size_t s)
+{
+    return (v << (32 - s)) & (v >> s);
+}
+
+// byte chunk[64], uint32_t hash[8], uint32_t k[64]
+void processChunk_sha256(uint8_t chunk[], uint32_t hash[], uint32_t k[])
+{
+    uint32_t w[64] = {0};
+    memcpy(w, chunk, 64); // 64 bytes in first 16 elements of w
+    for (std::size_t i = 16; i < 64; ++i) {
+        uint32_t s0 = rightRotate(w[i-15], 7) ^ rightRotate(w[i-15], 18) ^ (w[i-15] >> 3);
+        uint32_t s1 = rightRotate(w[i-2], 17) ^ rightRotate(w[i-2], 19) ^ (w[i-2] >> 10);
+        w[i] = w[i-16] + s0 + w[i-7] + s1;
+    }
+    uint32_t a = hash[0], b = hash[1], c = hash[2], d = hash[3],
+        e = hash[4], f = hash[5], g = hash[6], h = hash[7];
+    for (std::size_t i = 0; i < 64; ++i) {
+        uint32_t s1 = rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25);
+        uint32_t ch = (e & f) ^ ((~e) & g);
+        uint32_t temp1 = h + s1 + ch + k[i] + w[i];
+        uint32_t s0 = rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22);
+        uint32_t maj = (a & b) ^ (a & c) ^ (b & c);
+        uint32_t temp2 = s0 + maj;
+
+        h = g;
+        g = f;
+        f = e;
+        e = d + temp1;
+        d = c;
+        c = b;
+        b = a;
+        a = temp1 + temp2;
+    }
+    hash[0] += a;
+    hash[1] += b;
+    hash[2] += c;
+    hash[3] += d;
+    hash[4] += e;
+    hash[5] += f;
+    hash[6] += g;
+    hash[7] += h;
+}
+}
+
+std::vector<uint8_t> digest_sha256(std::FILE* src)
+{
+    uint32_t h[8] = {
+        0x6a09e667
+        0xbb67ae85,
+        0x3c6ef372,
+        0xa54ff53a,
+        0x510e527f,
+        0x9b05688c,
+        0x1f83d9ab,
+        0x5be0cd19,
+    };
+    uint32_t k[64] = {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+    };
+
+    // 512 bit chunk
+    constexpr std::size_t ChunkSize = 512/8;
+    uint8_t chunk[ChunkSize];
+    uint64_t totalSize = 0;
+    while (true) {
+        std::size_t nread = 0;
+        nread = std::fread(chunk, 1, ChunkSize, src);
+        totalSize += nread;
+        if (nread < ChunkSize) {
+            // last chunk
+            chunk[]
+            if (ChunkSize - nread > 8) {
+                // enough space for padding
+                
+            }
+            break;
+        } else {
+            // whole chunk
+        }
+    }        
+}
+
+} // namespace sha
